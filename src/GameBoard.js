cc2d55811b2e5a006e3e1c120306e6c6359deeb9
pswai <parksoon_my@hotmail.com>
2016-09-15 00:44:15 -0700

    done board

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -3,11 +3,11 @@ import { StyleSheet, css } from 'aphrodite';
 import GameCell from './GameCell';
 
 const GAME_MAP = [
-  ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'WIN'],
-  ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
-  ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
-  ['x', 'x', 3, 'x', 3, 1, 100, 'x', 'x', 'x'],
-  ['Start', 1, 1, 3, 2, 'x', 'x', 'x', 'x', 'x']
+  ['x', 'x', 'x', 5, 1, 1, 'WIN'],
+  ['x', 'x', 6, 20, 'x', 'x', 'x'],
+  ['x', 'x', 'x', 1, 1, 'x', 'x'],
+  ['x', 'x', 3, 'x', 3, 1, 100],
+  ['Start', 1, 1, 3, 2, 'x', 'x']
 ];
 const START_ROW = GAME_MAP.length - 1;
 const START_COL = 0;
